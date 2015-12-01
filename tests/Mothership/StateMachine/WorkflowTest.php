7b1e1a286713c641abe0be18408922256939a7c2
Maurizio Brioschi <brioschi@mothership.de>
2015-12-01 18:06:49 +0100

    code formatting

--- tests/Mothership/StateMachine/WorkflowTest.php
+++ tests/Mothership/StateMachine/WorkflowTest.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Magento
+ * Mothership GmbH
  *
  * NOTICE OF LICENSE
  *
@@ -11,16 +10,16 @@
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * to office@mothership.de so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
  * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
+ * needs please refer to http://www.mothership.de for more information.
  *
  * @category  Mothership
- * @package   Mothership_{EXTENSION NAME}
+ * @package   Mothership_state_machine
  * @author    Maurizio Brioschi <brioschi@mothership.de>
  * @copyright Copyright (c) 2015 Mothership GmbH
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -32,7 +31,15 @@ use Exemple\Simple\SimpleWorkflow;
 use Exemple\IfConditions\IfConditionsWorkflow;
 use Exemple\Fail\FailStateMachine;
 use Exemple\Fail\FailWorkflow;
-
+/**
+ * WorkflowTest
+ *
+ * @category  Mothership
+ * @package   Mothership_State_machine
+ * @author    Maurizio Brioschi <brioschi@mothership.de>
+ * @copyright 2015 Mothership GmbH
+ * @link      http://www.mothership.de/
+ */
 class WorkflowTest extends StateMachineTestCase
 {
     /**
